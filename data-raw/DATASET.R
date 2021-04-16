6dc67ff2c7bb4190c4dd58d81e334ca98aaf7d3a
Vineet Gupta <55761250+VineetG11@users.noreply.github.com>
2021-04-16 15:12:01 -0700

    create a plot_theme function

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,8 +1,48 @@
-ses_cat_colors <- c(
+
+# PLOT THEME --------------------------------------------------------------
+
+plot_theme <- function(plot) {
+  plot =
+    plot +
+    scale_x_discrete(expand = c(0.03, 0.03)) +
+    theme_bw() +
+    theme(
+      # Title
+      legend.title = element_blank(),
+      # Legend
+      legend.text = element_text(size = 9),
+      legend.position = "bottom",
+      # Caption
+      plot.caption = element_text(size = 7, hjust = 0, face = "italic"),
+      # X-axis
+      axis.ticks.x = element_blank(),
+      axis.title.x = element_blank(),
+      axis.text.x = element_text(size = 9),
+      # Y-axis
+      axis.ticks.y = element_blank(),
+      axis.title.y = element_text(size = 9),
+      # Background
+      panel.grid.minor.y = element_line(color = "grey80"),
+      panel.grid.major.x = element_blank(),
+      panel.background = element_blank(),
+      axis.line = element_line(colour = "black"),
+      panel.border = element_blank()) +
+    guides(color = guide_legend(nrow = 1))
+  return(plot)
+}
+
+
+# COLOR VECTORS -----------------------------------------------------------
+# all colors should be named "colors_[]" so it's easy for the user to find
+
+colors_ses <- c(
   "Low" = "#D1EAE8",
   "Moderate" = "#8CCAC5",
   "Middle" = "#30A097",
   "High" = "#147870")
 
+# these data ARE available to the user
+usethis::use_data(colors_ses, overwrite = TRUE)
 
-usethis::use_data(ses_cat_colors, overwrite = TRUE)#, internal = TRUE)
+# these data are NOT available to the user
+usethis::use_data(plot_theme, overwrite = TRUE, internal = TRUE)
