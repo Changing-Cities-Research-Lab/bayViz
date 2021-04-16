a942f708d86b122c7e1d3760efd09669329ae9f4
Vineet Gupta <55761250+VineetG11@users.noreply.github.com>
2021-04-16 14:28:16 -0700

    initialize bayViz package

---
+++ data-raw/DATASET.R
@@ -0,0 +1,8 @@
+ses_cat_colors <- c(
+  "Low" = "#D1EAE8",
+  "Moderate" = "#8CCAC5",
+  "Middle" = "#30A097",
+  "High" = "#147870")
+
+
+usethis::use_data(ses_cat_colors, overwrite = TRUE)#, internal = TRUE)
