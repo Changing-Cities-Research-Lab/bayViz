db3dd5264815ec4abdb1e5c3afed8e48236da93d
brooketran <btran1@stanford.edu>
2021-07-20 14:03:43 -0700

    added jenks breaks functionality

--- R/make_geo_map_panel.R
+++ R/make_geo_map_panel.R
@@ -146,7 +146,7 @@ make_geo_map_panel <- function(
   maps_all = list()
   panels = unique(data$panel_title)
   
-  # Get common legend
+  # Get common legend ----
   legend_map <-
     ggmap(gmap_sf) +
     geom_sf(
@@ -173,7 +173,7 @@ make_geo_map_panel <- function(
       panel.border = element_rect(colour = "black", fill=NA)
     )
   
-  # discrete color bar
+  # discrete color bar 
   if (jenksbreaks) {
     
     # set colors manually if different number of negative and positive bins
@@ -228,7 +228,7 @@ make_geo_map_panel <- function(
   
   title_letters = c("(a) ", "(b) ", "(c) ", "(d) ", "(e) ", "(f) ")
   
-  # Plot maps
+  # Plot maps ----
   foreach(i = 1:length(panels)) %do% {
     data_panel = data %>%
       dplyr::filter(panel_title == panels[i]) %>%
@@ -248,153 +248,152 @@ make_geo_map_panel <- function(
     } else if (panels[i] == "San Jose") {
       background_map = gmap_sj
     }
-    # # discrete color bar
-    # if (jenksbreaks) {
-    #   # Force each jenks break point into the data, assigned to a non-Oakland tract
-    #   # This prevents color palette from being used sequentially when certain breaks aren't present
-    #   
-    #   # Create column of breaks
-    #   var_null = breaks
-    #   
-    #   # Get tractid data for tracts not in specified geography
-    #   non_geo_tracts <-
-    #     shp_tracts %>%
-    #     filter(!GEOID10S %in% oak_ids$trtid10) %>%
-    #     mutate(tractid10 = GEOID10S) %>%
-    #     select(tractid10) %>%
-    #     st_drop_geometry()
-    #   
-    #   # Get tracts not in Oakland
-    #   tractid10 = non_oak_tracts[1:length(var_null),]
-    #   
-    #   # Create data frame
-    #   df = data.frame(tractid10, var_null) %>%
-    #     mutate(var = var_null) %>%
-    #     select(tractid10, var)
-    #   
-    #   # Get geometry data
-    #   df = shp_tracts %>%
-    #     right_join(df, by = c("GEOID10S" = "tractid10")) %>%
-    #     st_transform(CRS("+proj=longlat +datum=WGS84"))
-    #   
-    #   # Combine with original data frame
-    #   data_period = bind_rows(data_period, df)
-    #   
-    #   map <-
-    #     ggmap(gmap_oak) +
-    #     geom_sf(
-    #       data = data_period,
-    #       aes(fill = var),
-    #       size = 0,
-    #       alpha = 0.7,
-    #       inherit.aes = FALSE
-    #     ) +
-    #     geom_sf(
-    #       data = data_period,
-    #       size = 0.3,
-    #       alpha = 0,
-    #       inherit.aes = FALSE,
-    #       color = "black"
-    #     ) +
-    #     guides(
-    #       fill =
-    #         guide_colorbar(
-    #           barheight = 0.5,
-    #           barwidth = 15,
-    #           title = NULL,
-    #           frame.colour = "black"
-    #         )
-    #     ) +
-    #     theme_void() +
-    #     theme(
-    #       legend.title = element_blank(),
-    #       legend.position = "none",
-    #       plot.title = element_text(size = 12, hjust = .5, vjust = 3),
-    #       plot.margin = margin(3,-.5,3,-.5, unit = "pt"),
-    #       plot.caption = element_text(size = 8),
-    #       panel.border = element_rect(colour = "black", fill=NA)
-    #     ) +
-    #     labs(title = period_panels[i])
-    #   
-    #   # set colors manually if different number of negative and positive bins
-    #   if (neg_bins != pos_bins) {
-    #     # Custom palettes ***MUST SET CUSTOM COLORS
-    #     
-    #     # 1 negative, 3 positive bins:
-    #     if (neg_bins == 1 & pos_bins == 3) {
-    #       pal <- c("#67a9cf", "#fddbc7", "#ef8a62", "#b2182b")
-    #       # 2 negative, 3 positive bins:
-    #     } else if (neg_bins == 2 & pos_bins == 3) {
-    #       pal <- c("#2166ac", "#67a9cf", "#fddbc7", "#ef8a62", "#b2182b")
-    #     }
-    #     
-    #     scale_fill_fermenter_custom <- function(pal,
-    #                                             na.value = "grey60",
-    #                                             breaks,
-    #                                             labels) {
-    #       binned_scale(aesthetics = "fill",
-    #                    scale_name = "fermenter",
-    #                    palette = ggplot2:::binned_pal(scales::manual_pal(pal)),
-    #                    na.value = na.value,
-    #                    breaks = breaks,
-    #                    labels = labels)
-    #     }
-    #     map = map + scale_fill_fermenter_custom(pal,
-    #                                             breaks = breaks,
-    #                                             labels = labels)
-    #   } else {
-    #     map = map + scale_fill_fermenter(breaks = breaks,
-    #                                      type = type,
-    #                                      palette = palette,
-    #                                      direction = direction,
-    #                                      labels = labels)
-    #   }
-    #   
-    #   # gradient color scale
-    # } else {
     
-    map <-
-      ggmap(background_map) +
-      geom_sf(
-        data = data_panel,
-        aes(fill = var),
-        size = 0,
-        alpha = 0.7,
-        inherit.aes = FALSE
-      ) +
-      geom_sf(
-        data = data_panel,
-        size = 0.3,
-        alpha = 0,
-        inherit.aes = FALSE,
-        color = "black"
-      ) +
-      guides(
-        fill =
-          guide_colorbar(
-            barheight = 0.5,
-            barwidth = 15,
-            title = NULL,
-            frame.colour = "black"
-          )
-      ) +
-      theme_void() +
-      theme(
-        legend.title = element_blank(),
-        legend.position = "none",
-        plot.title = element_text(size = 12, hjust = .5, vjust = 3),
-        plot.margin = margin(3,-.5,3,-.5, unit = "pt"),
-        plot.caption = element_text(size = 8),
-        panel.border = element_rect(colour = "black", fill=NA)
-      ) +
-      labs(title = paste0(title_letters[i], panels[i]))
-    
-    map = map + scale_fill_gradientn(breaks = breaks,
-                                     labels = labels,
-                                     colors = alpha(MAP_COLORS, .8),
-                                     limits = range,
-                                     na.value = "grey60")
-    #}
+    ## Discrete color scale ----
+    if (jenksbreaks) {
+      # Force each jenks break point into the data, assigned to a non-Oakland tract
+      # This prevents color palette from being used sequentially when certain breaks aren't present
+      
+      # Create column of breaks
+      var_null = breaks
+      
+      # Get tractid data for tracts not in specified geography
+      non_geo_tracts <-
+        shp_tracts %>%
+        filter(!GEOID10S %in% data$trtid10) %>%
+        mutate(tractid10 = GEOID10S) %>%
+        select(tractid10) %>%
+        st_drop_geometry()
+      
+      # Get tracts not in specified geography
+      tractid10 = non_geo_tracts[1:length(var_null),]
+      
+      # Create data frame
+      df = data.frame(tractid10, var_null) %>%
+        mutate(var = var_null) %>%
+        select(tractid10, var)
+      
+      # Get geometry data
+      df = shp_tracts %>%
+        right_join(df, by = c("GEOID10S" = "tractid10")) %>%
+        st_transform(CRS("+proj=longlat +datum=WGS84"))
+      
+      # Combine with original data frame
+      data_panel = bind_rows(data_panel, df)
+      
+      map <-
+        ggmap(background_map) +
+        geom_sf(
+          data = data_panel,
+          aes(fill = var),
+          size = 0,
+          alpha = 0.7,
+          inherit.aes = FALSE
+        ) +
+        geom_sf(
+          data = data_panel,
+          size = 0.3,
+          alpha = 0,
+          inherit.aes = FALSE,
+          color = "black"
+        ) +
+        guides(
+          fill =
+            guide_colorbar(
+              barheight = 0.5,
+              barwidth = 15,
+              title = NULL,
+              frame.colour = "black"
+            )
+        ) +
+        theme_void() +
+        theme(
+          legend.title = element_blank(),
+          legend.position = "none",
+          plot.title = element_text(size = 12, hjust = .5, vjust = 3),
+          plot.margin = margin(3,-.5,3,-.5, unit = "pt"),
+          plot.caption = element_text(size = 8),
+          panel.border = element_rect(colour = "black", fill=NA)
+        ) +
+        labs(title = paste0(title_letters[i], panels[i]))
+      
+      # set colors manually if different number of negative and positive bins
+      if (neg_bins != pos_bins) {
+        # Custom palettes ***MUST SET CUSTOM COLORS
+        
+        # 1 negative, 3 positive bins:
+        if (neg_bins == 1 & pos_bins == 3) {
+          pal <- c("#67a9cf", "#fddbc7", "#ef8a62", "#b2182b")
+          # 2 negative, 3 positive bins:
+        } else if (neg_bins == 2 & pos_bins == 3) {
+          pal <- c("#2166ac", "#67a9cf", "#fddbc7", "#ef8a62", "#b2182b")
+        }
+        
+        scale_fill_fermenter_custom <- function(pal,
+                                                na.value = "grey60",
+                                                breaks,
+                                                labels) {
+          binned_scale(aesthetics = "fill",
+                       scale_name = "fermenter",
+                       palette = ggplot2:::binned_pal(scales::manual_pal(pal)),
+                       na.value = na.value,
+                       breaks = breaks,
+                       labels = labels)
+        }
+        map = map + scale_fill_fermenter_custom(pal,
+                                                breaks = breaks,
+                                                labels = labels)
+      } else {
+        map = map + scale_fill_fermenter(breaks = breaks,
+                                         type = type,
+                                         palette = palette,
+                                         direction = direction,
+                                         labels = labels)
+      }
+    } else {
+      # Gradient color scale ----
+      map <-
+        ggmap(background_map) +
+        geom_sf(
+          data = data_panel,
+          aes(fill = var),
+          size = 0,
+          alpha = 0.7,
+          inherit.aes = FALSE
+        ) +
+        geom_sf(
+          data = data_panel,
+          size = 0.3,
+          alpha = 0,
+          inherit.aes = FALSE,
+          color = "black"
+        ) +
+        guides(
+          fill =
+            guide_colorbar(
+              barheight = 0.5,
+              barwidth = 15,
+              title = NULL,
+              frame.colour = "black"
+            )
+        ) +
+        theme_void() +
+        theme(
+          legend.title = element_blank(),
+          legend.position = "none",
+          plot.title = element_text(size = 12, hjust = .5, vjust = 3),
+          plot.margin = margin(3,-.5,3,-.5, unit = "pt"),
+          plot.caption = element_text(size = 8),
+          panel.border = element_rect(colour = "black", fill=NA)
+        ) +
+        labs(title = paste0(title_letters[i], panels[i]))
+      
+      map = map + scale_fill_gradientn(breaks = breaks,
+                                       labels = labels,
+                                       colors = alpha(MAP_COLORS, .8),
+                                       limits = range,
+                                       na.value = "grey60")
+    }
     
     # plot coordinate points
     if (coord == T) {
@@ -409,8 +408,7 @@ make_geo_map_panel <- function(
     maps_all = c(maps_all, list(map))
   }
   
-  ## (EVENTUALLY: FACET_WRAP INSTEAD OF GRID.ARRANGE)
-  # arrange period maps into panels
+  ## Arrange maps into panels ----
   map_number <- length(panels)
   width = 0
   height = 0
